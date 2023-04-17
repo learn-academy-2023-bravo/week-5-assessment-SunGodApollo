54a3ee9c104410a15a5baebef11f1c399d903e73
SunGodApollo <apple.ortiz@hotmail.com>
2023-04-16 23:13:28 -0700

    finished week 5 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,39 +14,209 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+describe("secretString", () => {
+    it("takes in a string and returns a string with a coded message", () => {
+    const secretCodeWord1 = "Lackadaisical"
+    // Expected output: "L4ck4d41s1c4l"
+    const secretCodeWord2 = "Gobbledygook"
+    // Expected output: "G0bbl3dyg00k"
+    const secretCodeWord3 = "Eccentric"
+    // Expected output: "3cc3ntr1c"
+    expect(secretString(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+    expect(secretString(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+    expect(secretString(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+  })
+
+//   FAIL  ./code-challenges.test.js
+//   secretString
+//     ✕ takes in a string and returns a string with a coded message (1 ms)
+
+//   ● secretString › takes in a string and returns a string with a coded message
+
+//     ReferenceError: secretString is not defined
+
+
 
 // b) Create the function that makes the test pass.
+/* 
+  PSEUDOCODE
+    - create a function named secretString
+    - input: a string
+    - process: 
+        - make empty string variable
+        - set letter variable to index of string
+        - take string, iterate over string to look for specific characters: a, e, i, o, 
+        - if strings are equal to those characters, replace them with 4, 3, 1, 0
+    - output: words with converted strings replaced for those letters
+*/
+
+const secretString = (string) => {
+    let newString = ""
+    for(let index = 0; index < string.length; index++) {
+        let letter = string[index]
+        if(letter === 'a') {
+            newString += "4"
+        } else if(letter === 'e' || letter === 'E') {
+            newString += 3
+        } else if(letter === 'i') {
+            newString += '1'
+        } else if(letter === 'o') {
+            newString += '0'
+        } else {
+            newString += letter
+        }
+    }
+    return newString
+}
+
+// PASS  ./code-challenges.test.js
+// secretString
+//   ✓ takes in a string and returns a string with a coded message (3 ms)
+
+
+
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+describe("containsLetter", () => {
+    it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter", () => {
+        const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+        const letterA = "a"
+        // Expected output: ["Mango", "Apricot", "Peach"]
+        const letterE = "e"
+        // Expected output: ["Cherry", "Blueberry", "Peach"]
+    expect(containsLetter(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+    expect(containsLetter(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+  })
 
-const letterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+//   FAIL  ./code-challenges.test.js
+//   secretString
+//     ✓ takes in a string and returns a string with a coded message (3 ms)
+//   containsLetter
+//     ✕ takes in an array of words and a single letter and returns an array of all the words containing that particular letter
+
+//   ● containsLetter › takes in an array of words and a single letter and returns an array of all the words containing that particular letter
+
+//     ReferenceError: containsLetter is not defined
+
 
 // b) Create the function that makes the test pass.
+/*
+  PSEUDOCODE
+    - create a function named containsLetter
+    - input: an array
+    - process: 
+        - create function
+        - set empty array variable
+        - use .includes to check for string in array
+        - use .toLowerCase so uppercase letters don't mess it up
+        - use iteration method for
+        - if array includes letter at index, push what is at that index to empty array variable
+    - output: new array
+*/
+
+const containsLetter = (array, letter) => {
+    newArray = []
+    for(let index = 0; index < array.length; index++) {
+        if(array[index].toLowerCase().includes(letter.toLowerCase())) {
+            newArray.push(array[index])
+        }
+    }
+    return newArray
+}
+
+// PASS  ./code-challenges.test.js
+// secretString
+//   ✓ takes in a string and returns a string with a coded message (2 ms)
+// containsLetter
+//   ✓ takes in an array of words and a single letter and returns an array of all the words containing that particular letter (1 ms)
+
+
+
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
+describe("fullHouse", () => {
+    it('takes in an array of 5 numbers and determines whether or not the array is a "full house"', () => {
+        const hand1 = [5, 5, 5, 3, 3]
+        // Expected output: true
+        const hand2 = [5, 5, 3, 3, 4]
+        // Expected output: false
+        const hand3 = [5, 5, 5, 5, 4]
+        // Expected output: false
+        const hand4 = [7, 2, 7, 2, 7]
+        // Expected output: true
+    expect(fullHouse(hand1)).toEqual(true)
+    expect(fullHouse(hand2)).toEqual(false)
+    expect(fullHouse(hand3)).toEqual(false)
+    expect(fullHouse(hand4)).toEqual(true)
+    })
+  })
+
+//   FAIL  ./code-challenges.test.js
+//   secretString
+//     ✓ takes in a string and returns a string with a coded message (4 ms)
+//   containsLetter
+//     ✓ takes in an array of words and a single letter and returns an array of all the words containing that particular letter (2 ms)
+//   fullHouse
+//     ✕ takes in an array of 5 numbers and determines whether or not the array is a "full house" (1 ms)
+
+//   ● fullHouse › takes in an array of 5 numbers and determines whether or not the array is a "full house"
+
+//     ReferenceError: fullHouse is not defined
+
+
 
 // b) Create the function that makes the test pass.
+/* 
+ PSEUDOCODE
+  - create a function named fullHouse
+  - input: array
+  - process:
+        take in array
+        set empty object to keep track of how many times each number appears in the input array, key is the number, and the value is the number of times the number appears. 
+        iterate over array by using a for..of loop to add counts to the counts object
+        declare hasPair variable to false
+        declare hasThreeOfAKind variable to false
+        loop over counts object.values to see if the count of a number is 2 which is a pair, or 3 which is three of a kind.
+        if array is has threeOfAKind and hasPair, return true
+  - output: boolean
+*/
+const hand1 = [5, 5, 5, 3, 3]
+
+const fullHouse = (array) => {
+    const counts = {}
+    for(const num of array) {
+        if (counts[num]) {
+            counts[num]++
+        } else {
+            counts[num] = 1
+        }
+    }
+
+    let hasPair = false
+    let hasThreeOfAKind = false
+    for (const count of Object.values(counts)) {
+        if (count === 2) {
+            hasPair = true
+        } else if (count === 3) {
+            hasThreeOfAKind = true
+        }
+    }
+    return hasPair && hasThreeOfAKind
+}
+
+// PASS  ./code-challenges.test.js
+// secretString
+//   ✓ takes in a string and returns a string with a coded message (2 ms)
+// containsLetter
+//   ✓ takes in an array of words and a single letter and returns an array of all the words containing that particular letter (1 ms)
+// fullHouse
+//   ✓ takes in an array of 5 numbers and determines whether or not the array is a "full house" (1 ms)
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,6 +10,23 @@ letter_o = 'o'
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
+# PSEUDOCODE 
+    # create a method named new_array
+    # input: array, letter
+    # Process: 
+    # use .select block to filter the words array and return words that include the letter
+    # output a new array with the words containing letter argument
+
+def new_array(array, letter)
+    array.select do |value|
+        value.include?(letter)
+    end
+end
+
+p new_array(beverages_array, letter_t)
+
+
+
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
@@ -17,16 +34,68 @@ letter_t = 't'
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
+#PSEUDOCODE 
+# create a method named no_nested_array
+# input: hash
+# Process: use .values to access the values of the hash, then use flatten to get rid of nested arrays, then use sort to sort them in alphabetical order.
+# output: an array with no nested arrays, in alphabetical order. 
+
+def no_nested_array(hash)
+    hash.values.flatten.sort
+end
+
+p no_nested_array(us_states)
+
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
+ # PSEDUOCODE:
+ # creating a class called Bike, use attr_accessor, use model, wheels, and current speed.
+ # create bike_info method that interpolates movel, wheels, and current_speed into a sentence.
+class Bike
+    attr_accessor :model, :wheels, :current_speed
+    def initialize(model, wheels, current_speed)
+        @model = model
+        @wheels = 2
+        @current_speed = 0
+    end
 
+    def bike_info
+        "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+    end
+
+    def pedal_faster(amount)
+        @current_speed += amount
+    end
+
+    def brake(amount)
+        if amount < 0
+            @current_speed = 0
+        else 
+            @current_speed -= amount
+        end
+    end
+
+end
+my_bike = Bike.new('Mongoose', 0, 0)
+p my_bike.bike_info
 
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
+# PSEUDOCODE:
+# see above for 3b part of problem. 
+# created methods pedal_faster and brake
+# pedal faster takes in parameter amount which is the amount by how much faster the user wants to go, it then adds it to the @currentspeed
+# brake method is the same but instead of adding it subtracts the speed from the @current_speed. 
+
+
 # Expected output example: my_bike.pedal_faster(10) => 10
+p my_bike.pedal_faster(10)
 # Expected output example: my_bike.pedal_faster(18) => 28
+p my_bike.pedal_faster(18)
 # Expected output example: my_bike.brake(5) => 23
+p my_bike.brake(5)
 # Expected output example: my_bike.brake(25) => 0
+p my_bike.brake(25)
